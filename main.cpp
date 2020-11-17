33af06306774e54a7d6c16e8350ce07f337a2fea
AnnaGawrysiak <anna.m.gawrysiak@gmail.com>
2020-11-17 14:33:12 +0100

    add random choice of books

--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
-// przeladuj operator przypisania
-//Iteracja 2:
-//Dopisz do programu funkcje umożliwiające dodanie i usunięcie książki.
+/*
+
+Iteracja 4.
+Dodaj do powyższego panelu opcję
+e) szczęśliwy traf
+Czyli wylosuj dla użytkownika książkę do czytania
+*/
 #include <iostream>
 #include <string>
 #include <vector>
 #include <algorithm>    // std::sort
 #include <cmath>
+#include <ctime>
+#include <cstdlib>
 #include "Review.h"
 
 using namespace std;
@@ -13,20 +19,24 @@ using namespace std;
 bool WorseThan(const Review& r1, const Review& r2);
 bool FillReview(Review& rr);
 void ShowReview(const Review& rr);
+void userChoice(int choice);
 //bool operator==(Review const & lhs, Review const & rhs);
 //bool operator==(unsigned int other, Review const& r);
 //bool operator==(Review const& r, unsigned int other);
 
 int main()
 {
+     srand(time(NULL));
+
     vector<Review> books;
 
 	string title;
 
 	unsigned int rating = 0;
 
-    int choice;
-    cout << "Which do you want to do? (1) add a book title, (2)remove a book title,(3) display in alphabetical order, (4) display in ranking order, (5) exit "<< endl;
+    int choice = 0;
+    //userChoice(choice);
+    cout << "Which do you want to do? (1) add a book title, (2)remove a book title,(3) display in alphabetical order, (4) display in ranking order, (5) pick a randomly chosen book ,(6) exit "<< endl;
     cin >> choice;
 
     do
@@ -47,7 +57,9 @@ int main()
 
                 books.push_back(Book);
 
-                cout << "Which do you want to do? (1) add a book title, (2)remove a book title,(3) display in alphabetical order, (4) display in ranking order, (5) exit "<< endl;
+                //userChoice(choice);
+
+                cout << "Which do you want to do? (1) add a book title, (2)remove a book title,(3) display in alphabetical order, (4) display in ranking order, (5) pick a randomly chosen book, (6) exit "<< endl;
                 cin >> choice;
 
                 break;
@@ -69,15 +81,30 @@ int main()
                       books.erase(books.begin() + index);
                     }
 
-                cout << "Which do you want to do? (1) add a book title, (2)remove a book title,(3) display in alphabetical order, (4) display in ranking order, (5) exit "<< endl;
+                cout << "Which do you want to do? (1) add a book title, (2)remove a book title,(3) display in alphabetical order, (4) display in ranking order, (5) pick a randomly chosen book, (6) exit "<< endl;
                 cin >> choice;
                 break;
             }
 
         case 3:
             {
-                cout << "Whatever2";
-                cout << "Which do you want to do? (1) add a book title, (2)remove a book title,(3) display in alphabetical order, (4) display in ranking order, (5) exit "<< endl;
+               //b) w kolejnoœci alfabetycznej
+
+                // przy uzyciu wyrazenia lambda:
+
+                std::sort(books.begin(),
+                          books.end(),
+                          [](const Review & lhs, const Review & rhs)
+                          {return lhs.get_title() < rhs.get_title();});
+
+                cout << "Books in alphabetical order: ";
+
+                for(int unsigned i = 0; i < books.size(); i++)
+                    {
+                        ShowReview(books[i]);
+                    }
+
+                cout << "Which do you want to do? (1) add a book title, (2)remove a book title,(3) display in alphabetical order, (4) display in ranking order, (5) pick a randomly chosen book , (6) exit "<< endl;
                 cin >> choice;
 
                 break;
@@ -85,51 +112,53 @@ int main()
 
         case 4:
             {
-                cout << "Whatever2";
-                cout << "Which do you want to do? (1) add a book title, (2)remove a book title,(3) display in alphabetical order, (4) display in ranking order, (5) exit "<< endl;
+                //c) w kolejnoœci ocen malejaco
+
+                    std::sort(books.begin(), books.end(),
+                    [](const Review & lhs_, const Review & rhs_)
+                    {
+                        return lhs_.get_rating() > rhs_.get_rating();
+                    });
+
+                    cout << "The highest rated books: ";
+                    for(int unsigned i = 0; i < books.size(); i++)
+                        {
+                            ShowReview(books[i]);
+                        }
+
+                cout << "Which do you want to do? (1) add a book title, (2)remove a book title,(3) display in alphabetical order, (4) display in ranking order, (5) pick a randomly chosen book, (6) exit "<< endl;
                 cin >> choice;
                 break;
             }
 
+        case 5:
+            {
+                int randomIndex = rand() % books.size();
+                Review random_book = books.at(randomIndex);
+                cout << random_book.get_title() << " " << random_book.get_rating() << endl;
+                cout << "Which do you want to do? (1) add a book title, (2)remove a book title,(3) display in alphabetical order, (4) display in ranking order, (5) pick a randomly chosen book, (6) exit "<< endl;
+                cin >> choice;
+                break;
+            }
+
+            default:
+                {
+                    cout << "wrong entry. " <<
+                    endl << "Restarting program.";
+                    cout << "Which do you want to do? (1) add a book title, (2)remove a book title,(3) display in alphabetical order, (4) display in ranking order, (5) pick a randomly chosen book, (6) exit "<< endl;
+                    cin >> choice;
+                    //userChoice(choice); //Calls/restarts the function
+                    break;
+                }
+
         }
 
 
-    } while(choice != 5);
+    } while(choice != 6);
 
 //a) w kolejnosci podanej przez uzytkownika
 cout << "You entered the following:\n";
 	for(int unsigned i = 0; i < books.size(); i++)
-    {
-        ShowReview(books[i]);
-    }
-
-//b) w kolejnoœci alfabetycznej
-
-// przy uzyciu wyrazenia lambda:
-
-std::sort(books.begin(), books.end(),
-[](const Review & lhs, const Review & rhs)
-{
-    return lhs.get_title() < rhs.get_title();
-});
-
-cout << "Books in alphabetical order: ";
-for(int unsigned i = 0; i < books.size(); i++)
-    {
-        ShowReview(books[i]);
-    }
-
-
-//c) w kolejnoœci ocen malejaco
-
-std::sort(books.begin(), books.end(),
-[](const Review & lhs_, const Review & rhs_)
-{
-    return lhs_.get_rating() > rhs_.get_rating();
-});
-
-cout << "The highest rated books: ";
-for(int unsigned i = 0; i < books.size(); i++)
     {
         ShowReview(books[i]);
     }
@@ -155,6 +184,11 @@ void ShowReview(const Review & rr)
         cout << rr.get_rating() << endl;
 }
 
+void userChoice(int choice)
+{
+    cout << "Which do you want to do? (1) add a book title, (2)remove a book title,(3) display in alphabetical order, (4) display in ranking order, (5) pick a randomly chosen book, (6) exit "<< endl;
+                cin >> choice;
+}
 
 //bool operator==(Review const & lhs, Review const & rhs)
 //{
